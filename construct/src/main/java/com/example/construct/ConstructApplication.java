953502af285950ecdf76c24fea50fb91fc52bd32
kidreamx <kidreamx@gmail.com>
2024-06-01 16:49:55 +0900

    commit init

---
+++ construct/src/main/java/com/example/construct/ConstructApplication.java
@@ -0,0 +1,13 @@
+package com.example.construct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConstructApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConstructApplication.class, args);
+	}
+
+}
---
+++ construct/src/test/java/com/example/construct/ConstructApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.construct;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConstructApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
